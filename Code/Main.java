69cb5c6e3bb151f55f87a8a73dcbce2cb1b62d0e
eliaspfe <elias.pfeiffer@gmx.de>
2024-05-17 14:35:52 +0200

    added rest of dataset

--- Code/Main.java
+++ Code/Main.java
@@ -16,14 +16,30 @@ public class Main {
         rows.add(new Row("RX 7800", 800));
         rows.add(new Row("RX 7700", 650));
         rows.add(new Row("RTX 4090", 2130));
+        rows.add(new Row("RTX 4080", 1300));
+        rows.add(new Row("RTX 4070", 1000));
+        rows.add(new Row("RTX 4060", 780));
+        rows.add(new Row("RX 7900", 1300));
+        rows.add(new Row("RX 7800", 850));
+        rows.add(new Row("RX 7700", 830));
+        rows.add(new Row("RTX 4090", 1900));
+        rows.add(new Row("RTX 4080", 1350));
+        rows.add(new Row("RTX 4070", 1180));
+        rows.add(new Row("RTX 4060", 550));
+        rows.add(new Row("RX 7900", 1350));
+
         aggregatedRows = aggregator.aggregate(rows);
 
+
+        // Hier können die Aggregated Rows Ausgegeben werden
         //for(AggregatedRow row : aggregatedRows){
           //  System.out.println(row.grafikkarte + " " + row.gesamtPreis);
         //}
         ArrayList<AggregatedRow> sortedRows = Sorter.sort(aggregatedRows);
+
+        //Hier kann die Sortierte Liste ausgegeben werden
         for (AggregatedRow row : sortedRows) {
-            System.out.println(row.grafikkarte + ": " + row.gesamtPreis + " €");
+            System.out.println("Modell: " + row.grafikkarte + ", Gesamtpreis: " + row.gesamtPreis + " €");
         }
 
 
